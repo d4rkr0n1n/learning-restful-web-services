35cd920a2f0d55aca37b49fb9b3d864e1549ea87
Mridul Roy <midlry.mr@gmail.com>
2021-02-27 11:06:04 +0530

    + HelloWorldBean

--- src/main/java/com/java/training/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/java/training/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.java.training.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,16 @@ public class HelloWorldController {
 	
 	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World";
+		return "Hello World (Using RequestMapping)";
 	}
-
+	
+	@GetMapping(path = "hello-world-get")
+	public String helloWorldG() {
+		return "Hello World (Using GetMapping)";
+	}
+	
+	@GetMapping(path = "hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("This is the message from HelloWorldBean!!");
+	}
 }
